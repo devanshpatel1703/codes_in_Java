38f8471f6d357b59e413ba0b9ab32064ff86e1cb
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-06-15 18:58:36 +0530

    Create binary_addition_in_java.java

---
+++ binary_addition_in_java.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        boolean a=true;
+        int b1=0b1110;
+        int b2=0b1111;
+        System.out.print(b1+b2);
+    }
+}
