4279d433cac466fccdd3580cd8bcf56a8e769dbb
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-06-15 23:23:33 +0530

    Create string_input.java

---
+++ string_input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input=new Scanner(System.in);
+	    System.out.print("Enter your string : ");
+	    String str=input.next();
+	    System.out.print("String you entered : "+str);
+	}
+}
