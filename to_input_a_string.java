10153cddc2d8c032ca7c54a3963316a3ed4d8443
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-06-17 16:45:17 +0530

    Create to_input_a_string.java

---
+++ to_input_a_string.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String str;
+        System.out.print("Enter the String : ");
+        str=input.nextLine();
+        System.out.printf("The string is = %s",str);
+    }
+}
