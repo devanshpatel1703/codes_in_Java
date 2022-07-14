a8c433720c9c272cfae7d5d7d9a11cdbc8528a61
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-07-14 16:34:16 +0530

    Create converting_integer_into_string.java

---
+++ converting_integer_into_string.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        int n=input.nextInt();
+        String s=Integer.toString(n);
+        System.out.printf("Good job");
+    }
+}
