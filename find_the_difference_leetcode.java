590a312319f2e2fc98565b57fc7c85fe3f6a517b
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-09-03 13:00:00 +0530

    Create find_the_difference_leetcode.java

---
+++ find_the_difference_leetcode.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] a1 = s.toCharArray();
+        char[] a2 = t.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        for(int i=0;i<a1.length;i++){
+            if(a1[i]!=a2[i]){
+                return (a2[i]);
+            }
+        }
+        return (a2[t.length()-1]);
+    }
+}
