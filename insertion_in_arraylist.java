e3709aa9b3d8d186ae2247c6edad0268d2f064ef
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-10-24 13:44:57 +0530

    Create insertion_in_arraylist.java

---
+++ insertion_in_arraylist.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<String>();
+        list.add("red");
+        list.add("orange");
+        list.add("blue");
+        list.add("green");
+        list.add("yellow");
+        for(String a:list){
+            System.out.printf("%s\n",a);
+        }
+        System.out.printf("The new array list is :\n");
+        list.add(0,"black");
+        for(String a:list){
+            System.out.printf("%s\n",a);
+        }
+    }
+}
