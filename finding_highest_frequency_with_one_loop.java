85fecafe3a37d90f0945fadcea502a2ca5a32bf7
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-11-18 14:37:49 +0530

    Create finding_highest_frequency_with_one_loop.java

---
+++ finding_highest_frequency_with_one_loop.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.printf("Enter the size of array :");
+        int n=input.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=input.nextInt();
+        }
+        int ans=0;
+        int count=1;
+        for(int i=0;i<a.length-1;i++){
+            if(count>ans){
+                ans=count;
+            }
+            if(a[i]==a[i+1]){
+                count++;
+            }
+            else{
+                count=1;
+            }
+        }
+        System.out.printf("Largest count=%d",ans);
+    }
+}
